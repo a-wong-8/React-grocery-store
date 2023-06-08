aaaddf5e8b1d5293c6b3db83562c2e3b16be7afa
vincentpham123 <vincentpham32@gmail.com>
2023-06-08 14:32:52 -0700

    finished phase 1

--- react-redux-grocery-store/src/index.js
+++ react-redux-grocery-store/src/index.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import {Provider} from 'react-redux';
 import './index.css';
 import App from './App';
+import configureStore from './store';
+
+const store = configureStore();
 
 function Root() {
+  if (process.env.NODE_ENV !== "production") {
+    window.store = store;
+  }
   return (
+    <Provider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
+    </Provider>
   );
 }
 
